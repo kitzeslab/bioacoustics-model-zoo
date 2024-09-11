55c8e38ebc200b63cadc5b5fe9c0e1feaf7aa566
sammlapp <sammlapp@gmail.com>
2024-09-11 11:44:36 -0400

    update to Perch v8
    also support perch v4
    
    adds forward() method to get all outputs, including spectrogram, embeddings, and logits at all taxonomic levels

--- bioacoustics_model_zoo/perch.py
+++ bioacoustics_model_zoo/perch.py
@@ -16,61 +16,62 @@ from bioacoustics_model_zoo.utils import (
 
 
 class Perch(CNN):
-    """load Perch (aka Google Bird Vocalization Classifier) from TensorFlow Hub or local file
 
-    [Perch](https://tfhub.dev/google/bird-vocalization-classifier/4) is shared under the
-    [Apache 2.0 License](https://opensource.org/license/apache-2-0/).
+    def __init__(self, version=8, path=None):
+        """load Perch (aka Google Bird Vocalization Classifier) from TensorFlow Hub or local file
 
-    The model can be used to classify bird vocalizations from about 10,000 bird species, or
-    to generate feature embeddings for audio files. It was trained on recordings from Xeno Canto.
+        [Perch](https://www.kaggle.com/models/google/bird-vocalization-classifier/TensorFlow2/bird-vocalization-classifier/8)
+        is shared under the [Apache 2.0 License](https://opensource.org/license/apache-2-0/).
 
-    Model performance is described in :
-    ```
-    Ghani, Burooj, et al.
-    "Feature embeddings from large-scale acoustic bird classifiers enable few-shot transfer learning."
-    arXiv preprint arXiv:2307.06292 (2023).
-    ```
+        The model can be used to classify bird vocalizations from about 10,000 bird species, or
+        to generate feature embeddings for audio files. It was trained on recordings from Xeno Canto.
 
-    Args:
-        model_dir: path to local folder containing /savedmodel/saved_model.pb and /label.csv
-
-    Returns:
-        object with .predict(), .generate_embeddings() etc methods
-
-    Methods:
-        predict: get per-audio-clip per-class scores as pandas DataFrame
-        embed: generate embedding layer outputs for samples; optionally return logits as well
-
-    Example 1: download from TFHub and generate logits and embeddings
-    ```
-    import torch
-    model=torch.hub.load('kitzeslab/bioacoustics_model_zoo', 'Perch',trust_repo=True)
-    predictions = model.predict(['test.wav']) #predict on the model's classes
-    embeddings = model.embed(['test.wav']) #generate embeddings on each 5 sec of audio
-    ```
-
-    Example 2: loading from local folder
-    ```
-    m = torch.hub.load(
-        'kitzeslab/bioacoustics-model-zoo:google_bird_model',
-        'Perch',
-        url='/path/to/perch_0.1.2/',
-        trust_repo=True
-    )
-    """
-
-    def __init__(self, url="https://tfhub.dev/google/bird-vocalization-classifier/4"):
-        """load TF model hub google Perch model, wrap in OpSo TensorFlowHubModel class
+        Model performance is described in :
+        ```
+        Ghani, Burooj, et al.
+        "Feature embeddings from large-scale acoustic bird classifiers enable few-shot transfer learning."
+        arXiv preprint arXiv:2307.06292 (2023).
+        ```
 
         Args:
-            url: url to model path on TensorFlow Hub (default is Perch v4),
-                OR path to local folder containing /savedmodel/saved_model.pb and /label.csv
+            version: [default: 8], supports versions 3 & 4 (outputs (logits, embeddings))
+                and version 8 (outputs dictionary with keys 'order', 'family',
+                'genus', 'label', 'embedding', 'frontend')
+            path: url to model path on TensorFlow Hub (default is Perch v4),
+                OR path to local _folder_ containing /savedmodel/saved_model.pb
+                and /label.csv
+                [default: None loads from TFHub based on version]
+                Note: adjust `version` argument to match the model version in the local folder
 
-        Returns:
-            object with .predict(), .embed() etc methods
+        Methods:
+            predict: get per-audio-clip per-class scores as pandas DataFrame
+            embed: generate embedding layer outputs for samples
+            forward: return all outputs as a dictionary with keys
+                ('order', 'embedding', 'family', 'frontend', 'genus', 'label'):
+                - order, family, genus, and label (species) are logits at
+                    different taxonomic levels
+                - embedding is the feature vector from the penultimate layer
+                - frontend is the log mel spectrogram generated at the input
+                    layer
+
+        Example 1: download from TFHub and generate logits and embeddings
+        ```
+        import torch
+        model=torch.hub.load('kitzeslab/bioacoustics_model_zoo', 'Perch',trust_repo=True)
+        predictions = model.predict(['test.wav']) #predict on the model's classes
+        embeddings = model.embed(['test.wav']) #generate embeddings on each 5 sec of audio
+        ```
+
+        Example 2: loading from local folder
+        ```
+        m = torch.hub.load(
+            'kitzeslab/bioacoustics-model-zoo',
+            'Perch',
+            url='/path/to/perch_folder/',
+            trust_repo=True
+        )
         """
         # only require tensorflow and tensorflow_hub if/when this class is used
-        # this didn't work - could put try/except block at top, then check tf is availble here
         try:
             import tensorflow as tf
             import tensorflow_hub
@@ -81,6 +82,16 @@ class Perch(CNN):
                 "Install in your python environment with `pip install tensorflow tensorflow_hub`"
             ) from exc
 
+        tfhub_paths = {
+            4: "https://www.kaggle.com/models/google/bird-vocalization-classifier/TensorFlow2/bird-vocalization-classifier/4",
+            8: "https://www.kaggle.com/models/google/bird-vocalization-classifier/TensorFlow2/bird-vocalization-classifier/8",
+        }
+        self.version = version
+
+        if path is None:
+            path = tfhub_paths[version]
+
+        # Perch expects audio signal input as 32kHz mono 5s clips (160,000 samples)
         self.preprocessor = AudioPreprocessor(sample_duration=5, sample_rate=32000)
         self.sample_duration = 5
 
@@ -94,43 +105,95 @@ class Perch(CNN):
         self.inference_dataloader_cls = SafeAudioDataloader
 
         # Load pre-trained model: handle url from tfhub or local dir
-        if urllib.parse.urlparse(url).scheme in ("http", "https"):
+        if urllib.parse.urlparse(path).scheme in ("http", "https"):
             # its a url, load from tfhub
-            self.network = tensorflow_hub.load(url)
+            self.network = tensorflow_hub.load(path)
 
-            # load class list
-            label_csv = tensorflow_hub.resolve(url) + "/assets/label.csv"
-            self.classes = pd.read_csv(label_csv)["ebird2021"].values
+            # load lists of taxonomic levels
+            label_csv = tensorflow_hub.resolve(path) + "/assets/label.csv"
+            order_csv = tensorflow_hub.resolve(path) + "/assets/order.csv"
+            family_csv = tensorflow_hub.resolve(path) + "/assets/family.csv"
+            genus_csv = tensorflow_hub.resolve(path) + "/assets/genus.csv"
+
         else:
-            # must be a local directory containing /savedmodel/saved_model.pb and /label.csv
-            assert Path(url).is_dir(), f"url {url} is not a directory or a URL"
+            # must be a local directory containing /savedmodel/saved_model.pb and
+            # csvs: label.csv, order.csv, family.csv, genus.csv listing class names
+            assert Path(path).is_dir(), f"url {path} is not a directory or a URL"
             # tf.saved_model.load looks for `saved_model.pb` file in the directory passed to it
-            self.network = tf.saved_model.load(Path(url) / "savedmodel")
-            # load class list
-            self.classes = pd.read_csv(Path(url) / "label.csv")["ebird2021"].values
+            self.network = tf.saved_model.load(Path(path) / "savedmodel")
+            # load lists of taxonomic levels
+            label_csv = Path(path) / "label.csv"
+            order_csv = Path(path) / "assets/order.csv"
+            family_csv = Path(path) / "assets/family.csv"
+            genus_csv = Path(path) / "assets/genus.csv"
+
+        self.taxonomic_classes = {
+            "order": pd.read_csv(order_csv)["ebird2021_orders"].values,
+            "family": pd.read_csv(family_csv)["ebird2021_families"].values,
+            "genus": pd.read_csv(genus_csv)["ebird2021_genera"].values,
+            "species": pd.read_csv(label_csv)["ebird2021"].values,
+        }
+        self.classes = self.taxonomic_classes["species"]
 
         self.device = opensoundscape.ml.cnn._gpu_if_available()
 
     def __call__(
-        self, dataloader, wandb_session=None, progress_bar=True, return_embeddings=False
+        self, dataloader, wandb_session=None, progress_bar=True, return_value="logits"
     ):
         """run forward pass of model, iterating through dataloader batches
 
         Args:
             dataloader: instance of SafeAudioDataloader or custom subclass
+            wandb_session: wandb.Session object, if provided, logs progress
+            progress_bar: bool, if True, shows a progress bar with tqdm [default: True]
+            return_value: str, 'logits', 'embeddings', 'taxonomic', or 'all' [default: 'logits']
+                - 'logits': returns only the logits on the species classes
+                - 'embeddings': returns only the feature vectors from the penultimate layer
+                - 'taxonomic': returns dictionionary with logits on all taxonomic levels
+                    (keys: 'order', 'family', 'genus', 'label')
+                - 'all': returns dictionary with all Perch outputs
+                    (keys: 'order', 'family', 'genus', 'label', 'embedding', 'frontend')
+                    - 'label' is the logits on the species classes
+                    - 'embedding' is the feature vectors from the penultimate layer
+                    - 'frontend' is the log mel spectrogram generated at the input layer
+                Note: for version < 8, 'order', 'family', 'genus', and 'frontend' will be None
+
 
         Returns: logits, or (logits, embeddings) if return_embeddings=True
             logits: numpy array of shape (n_samples, n_classes)
             embeddings: numpy array of shape (n_samples, n_features)
         """
+        # check return_value argument is valid
+        err = f"return_value must be one of 'logits', 'embeddings', 'taxonomic', or 'all', got {return_value}"
+        assert return_value in ("logits", "embeddings", "taxonomic", "all"), err
+
         # iterate batches, running inference on each
         logits = []
         embeddings = []
-        # logmelspec = []
+        order_logits = []
+        family_logits = []
+        genus_logits = []
+        logmelspec = []
         for i, samples_batch in enumerate(tqdm(dataloader, disable=not progress_bar)):
-            batch_logits, batch_embeddings = self.network.infer_tf(samples_batch)
-            logits.extend(batch_logits.numpy().tolist())
-            embeddings.extend(batch_embeddings.numpy().tolist())
+            outs = self.network.infer_tf(samples_batch)
+            if self.version < 8:
+                # earlier versions just output (logits, embeddings)
+                if return_value != "embeddings":
+                    logits.extend(outs[0].numpy().tolist())
+                if return_value != "logits":
+                    embeddings.extend(outs[1].numpy().tolist())
+            else:
+                # only aggregate the outputs requested to save memory
+                if return_value != "embeddings":
+                    logits.extend(outs["label"].numpy().tolist())
+                if return_value != "logits":
+                    embeddings.extend(outs["embedding"].numpy().tolist())
+                if return_value in ("taxonomic", "all"):
+                    order_logits.extend(outs["order"].numpy().tolist())
+                    family_logits.extend(outs["family"].numpy().tolist())
+                    genus_logits.extend(outs["genus"].numpy().tolist())
+                if return_value == "all":
+                    logmelspec.extend(outs["frontend"].numpy().tolist())
 
             if wandb_session is not None:
                 wandb_session.log(
@@ -140,9 +203,25 @@ class Perch(CNN):
                         "total_batches": len(dataloader),
                     }
                 )
-        if return_embeddings:
-            return (np.array(logits), np.array(embeddings))
-        else:
+        if return_value == "taxonomic":
+            return {
+                "order": np.array(order_logits) if self.version >= 8 else None,
+                "family": np.array(family_logits) if self.version >= 8 else None,
+                "genus": np.array(genus_logits) if self.version >= 8 else None,
+                "label": np.array(logits),
+            }
+        elif return_value == "all":
+            return {
+                "order": np.array(order_logits) if self.version >= 8 else None,
+                "family": np.array(family_logits) if self.version >= 8 else None,
+                "genus": np.array(genus_logits) if self.version >= 8 else None,
+                "label": np.array(logits),
+                "embedding": np.array(embeddings),
+                "frontend": np.array(logmelspec) if self.version >= 8 else None,
+            }
+        elif return_value == "embeddings":
+            return np.array(embeddings)
+        elif return_value == "logits":
             return np.array(logits)
 
     def predict_dataloader(self, samples, **kwargs):
@@ -155,6 +234,71 @@ class Perch(CNN):
         return super().predict_dataloader(
             samples=samples, collate_fn=collate_to_np_array, **kwargs
         )
+
+    def forward(self, samples, progress_bar=True, wandb_session=None):
+        """
+        Run inference on a list of samples, returning all outputs as a dictionary
+
+        Args:
+            samples: list of file paths, OR pd.DataFrame with index containing audio file paths
+            progress_bar: bool, if True, shows a progress bar with tqdm [default: True]
+            wandb_session: wandb.Session object, if provided, logs progress
+
+        Returns: dictionary with keys ('order', 'embedding', 'family', 'frontend', 'genus', 'label'):
+            - order, family, genus, and label (species) are dataframes of logits at different taxonomic levels
+            - embedding is a dataframe with the feature vector from the penultimate layer
+            - frontend is np.array of log mel spectrograms generated at the input layer
+        """
+        # create dataloader to generate batches of AudioSamples
+        dataloader = self.predict_dataloader(samples)
+
+        # run inference, getting all outputs
+        outs = self(
+            dataloader=dataloader,
+            wandb_session=wandb_session,
+            progress_bar=progress_bar,
+            return_value="all",
+        )
+
+        # put logit & embedding outputs in DataFrames with multi-index like .predict()
+        outs["order"] = (
+            pd.DataFrame(
+                data=outs["order"],
+                index=dataloader.dataset.dataset.label_df.index,
+                columns=self.taxonomic_classes["order"],
+            )
+            if self.version >= 8
+            else None
+        )
+        outs["family"] = (
+            pd.DataFrame(
+                data=outs["family"],
+                index=dataloader.dataset.dataset.label_df.index,
+                columns=self.taxonomic_classes["family"],
+            )
+            if self.version >= 8
+            else None
+        )
+        outs["genus"] = (
+            pd.DataFrame(
+                data=outs["genus"],
+                index=dataloader.dataset.dataset.label_df.index,
+                columns=self.taxonomic_classes["genus"],
+            )
+            if self.version >= 8
+            else None
+        )
+        outs["label"] = pd.DataFrame(
+            data=outs["label"],
+            index=dataloader.dataset.dataset.label_df.index,
+            columns=self.taxonomic_classes["species"],
+        )
+        outs["embedding"] = pd.DataFrame(
+            data=outs["embedding"],
+            index=dataloader.dataset.dataset.label_df.index,
+        )
+
+        return outs
 
     def embed(
         self,
@@ -181,16 +325,31 @@ class Perch(CNN):
             types are pd.DataFrame if return_dfs=True, or np.array if return_dfs=False
             - preds are always logit scores with no activation layer applied
             - embeddings are the feature vectors from the penultimate layer of the network
+
+        See also:
+            - predict: get per-audio-clip species outputs as pandas DataFrame
+            - forward: return all outputs as a dictionary with keys
         """
         # create dataloader to generate batches of AudioSamples
         dataloader = self.predict_dataloader(samples, **kwargs)
 
-        # run inference, returns (scores, intermediate_outputs)
-        preds, embeddings = self(
-            dataloader=dataloader,
-            progress_bar=progress_bar,
-            return_embeddings=True,
-        )
+        # run inference, getting embeddings and optionally logits
+        if return_preds:
+            # get all outputs, then extract embeddings and logits
+            outs = self(
+                dataloader=dataloader,
+                progress_bar=progress_bar,
+                return_value="all",
+            )
+            embeddings = outs["embedding"]
+            preds = outs["label"]
+        else:
+            # save memory by only aggergating the embeddigs
+            embeddings = self(
+                dataloader=dataloader,
+                progress_bar=progress_bar,
+                return_value="embeddings",
+            )
 
         if return_dfs:
             # put embeddings in DataFrame with multi-index like .predict()
